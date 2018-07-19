1cc1a8ff44b87ba146060d4143ef37a6eb4ff42a
Jacob Laframboise <jlaframboise02@gmail.com>
2018-07-19 10:06:56 -0400

    The ProcessCurvatureData.py file now has functions to add the count and percentage of length details to a text file containing just the curvature values, and can combine multiple scan positions' curvature files for easy import to Excel.

--- ProcessCurvatureData.py
+++ ProcessCurvatureData.py
@@ -1,4 +1,4 @@
-def addPctCount(inPath, outputPath):
+def addDetails(inPath, outputPath):
 	'''A function that takes the path of a text file and creates a new text file with the point number,
 	and the percentage of how far the point is along the list. Easy to import to Excel'''
 	inFile = open(inPath, 'r')
@@ -12,33 +12,72 @@ def addPctCount(inPath, outputPath):
 	inFile.close()
 	outFile.close()
 	
-#addPctCount(r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\PTBD0033_ProCurvatures.txt",r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\testing.txt")
+#addDetails(r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\PTBD0033_ProCurvatures.txt",r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\testing.txt")
 
-def addManyPctCount(inPathList):
+def addManyDetails(inPathList):
 	for path in inPathList:
 		try:
 			outPath = path[:-4] + 'Data.txt'
-			addPctCount(path, outPath)
+			addDetails(path, outPath)
 			#print('In: {} \n Out: {}'.format(path, outPath))
 		except:
 			pass
 
+def combineDataFiles(inFileList, outputPath):
+	filesData = []
+	for x in inFileList:
+		try:
+			f = open(x, 'r')
+			lines = f.readlines()
+			filesData.append(lines)
+			f.close()
+		except:
+			7*2
+			pass
+	filesData.sort(key=len, reverse=True)
+	
+	outFile = open(outputPath, 'w')
+	
+	for x in range(len(filesData[0])): #this will be for every item in each list (going down the rows)
+		toPrint = []
+		for count, item in enumerate(filesData):
+			try:
+				toPrint.append(item[x])
+			except: 
+				7*2
+				pass
+		toPrint = [x.strip() for x in toPrint]
+		toPrint = ', '.join(toPrint)
+		outFile.write(toPrint + '\n')
 
-		
-#fileList = [r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\PTBD0033_ProCurvatures.txt",
-#r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\PTBD0033\PTBD0033_SupCurvatures.txt"]
-
+def combinePatientDataFiles(patPath):
+	supPath = patPath + '\\' + patPath[-8:] + '_SupCurvaturesData.txt'
+	proPath = patPath + '\\' + patPath[-8:] + '_ProCurvaturesData.txt'
+	leftDownPath = patPath + '\\' + patPath[-8:] + '_LeftDownCurvaturesData.txt'
+	outPath = patPath + '\\' + patPath[-8:] + '_AllCurvaturesData.txt'
+	
+	combineDataFiles([supPath, proPath, leftDownPath], outPath)
+	
+	
+	
+	
 idList = ['PTAF0056', 'PTAJ0023', 'PTAJ0095', 'PTAM0029', 'PTAP0049', 'PTAT0093', 'PTBB0002', 'PTBB0024', 'PTBC0016', 'PTBC0017', 'PTBD0033', 'PTBG0026', 'TEST0012']
-#idList = idList[:2]
+directory = r'C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\\' 
 
-fileListPro = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_ProCurvatures.txt" for id in idList]
-fileListSup = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_SupCurvatures.txt" for id in idList]
-fileListLeftDown = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_LeftDownCurvatures.txt" for id in idList]
+for id in idList:
+	patPath = directory + id
+	combinePatientDataFiles(patPath)
+	
+#combinePatientDataFiles(r'C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\TEST0012')
+		
+		
+		
 
-#print(fileList)
-
-addManyPctCount(fileListPro)
-
-addManyPctCount(fileListSup)
-
-addManyPctCount(fileListLeftDown)
+#The following 6 lines create more detailed data files for all the patients:
+#idList = ['PTAF0056', 'PTAJ0023', 'PTAJ0095', 'PTAM0029', 'PTAP0049', 'PTAT0093', 'PTBB0002', 'PTBB0024', 'PTBC0016', 'PTBC0017', 'PTBD0033', 'PTBG0026', 'TEST0012']
+#fileListPro = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_ProCurvatures.txt" for id in idList]
+#fileListSup = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_SupCurvatures.txt" for id in idList]
+#fileListLeftDown = ["C:\\Users\\jlaframboise\\Documents\\ColonCurves_JL\CtVolumes\\" + id + "\\" + id + "_LeftDownCurvatures.txt" for id in idList]
+#addManyDetails(fileListPro)
+#addManyDetails(fileListSup)
+#addManyDetails(fileListLeftDown)
