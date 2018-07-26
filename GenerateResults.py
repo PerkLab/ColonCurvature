e9e2bee1577c1d2b5bfa2e5ac0c7e2c2ea652dd9
Jacob Laframboise <jlaframboise02@gmail.com>
2018-07-26 09:54:35 -0400

    The GenerateResults.py file now can output a nice csv style text file for easy import into Excel, and it looks great. Easy to compare stats on supine and prone of the full colon.

--- GenerateResults.py
+++ GenerateResults.py
@@ -81,6 +81,52 @@ def getStats(dataInPath):
 	lessThan160DegDists = [str(x[1]) for x in sorted(lessThan160Deg, key = itemgetter(1))]
 	lessThan180DegDists = [str(x[1]) for x in sorted(lessThan180Deg, key = itemgetter(1))]
 	
+	if lessThan20Deg:
+		lessThan20DegAvgDist = stat.mean([float(x[1]) for x in lessThan20Deg])
+	else:
+		lessThan20DegAvgDist = 0
+		
+	if lessThan40Deg:
+		lessThan40DegAvgDist = stat.mean([float(x[1]) for x in lessThan40Deg])
+	else:
+		lessThan40DegAvgDist = 0
+		
+	if lessThan60Deg:
+		lessThan60DegAvgDist = stat.mean([float(x[1]) for x in lessThan60Deg])
+	else:
+		lessThan60DegAvgDist = 0
+		
+	if lessThan80Deg:
+		lessThan80DegAvgDist = stat.mean([float(x[1]) for x in lessThan80Deg])
+	else:
+		lessThan80DegAvgDist = 0
+		
+	if lessThan100Deg:
+		lessThan100DegAvgDist = stat.mean([float(x[1]) for x in lessThan100Deg])
+	else:
+		lessThan100DegAvgDist = 0
+		
+	if lessThan120Deg:
+		lessThan120DegAvgDist = stat.mean([float(x[1]) for x in lessThan120Deg])
+	else:
+		lessThan120DegAvgDist = 0
+		
+	if lessThan140Deg:
+		lessThan140DegAvgDist = stat.mean([float(x[1]) for x in lessThan140Deg])
+	else:
+		lessThan140DegAvgDist = 0
+		
+	if lessThan160Deg:
+		lessThan160DegAvgDist = stat.mean([float(x[1]) for x in lessThan160Deg]) 
+	else:
+		lessThan160DegAvgDist = 0
+		
+	if lessThan180Deg:
+		lessThan180DegAvgDist = stat.mean([float(x[1]) for x in lessThan180Deg])
+	else:
+		lessThan180DegAvgDist = 0
+
+	
 	meanCurveDegrees = stat.mean(allCurveDegrees)
 	meanCurveDistance = stat.mean(allCurveDistances)
 	medianCurveDegrees = stat.median(allCurveDegrees)
@@ -106,22 +152,39 @@ def getStats(dataInPath):
 	
 	
 	linesOut.append('Number of curves < 20deg, {}'.format(len(lessThan20Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan20DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan20DegDists)))
+	
 	linesOut.append('Number of curves < 40deg, {}'.format(len(lessThan40Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan40DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan40DegDists)))
+	
 	linesOut.append('Number of curves < 60deg, {}'.format(len(lessThan60Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan60DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan60DegDists)))
+	
 	linesOut.append('Number of curves < 80deg, {}'.format(len(lessThan80Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan80DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan80DegDists)))
+	
 	linesOut.append('Number of curves < 100deg, {}'.format(len(lessThan100Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan100DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan100DegDists)))
+	
 	linesOut.append('Number of curves < 120deg, {}'.format(len(lessThan120Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan120DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan120DegDists)))
+	
 	linesOut.append('Number of curves < 140deg, {}'.format(len(lessThan140Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan140DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan140DegDists)))
+	
 	linesOut.append('Number of curves < 160deg, {}'.format(len(lessThan160Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan160DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan160DegDists)))
+	
 	linesOut.append('Number of curves < 180deg, {}'.format(len(lessThan180Deg)))
+	linesOut.append('Mean Distance of Curves ^, {}'.format(str(lessThan180DegAvgDist)))
 	linesOut.append('Curve Distances, {}'.format(' '.join(lessThan180DegDists)))
 	linesOut.append('')
 	
@@ -149,8 +212,9 @@ def getStats(dataInPath):
 	
 	
 def compareSupineProne(patPath):
-	supPath = os.path.join(patPath, patPath[-8:] + '_SupCurvaturesDataResults.txt')
-	proPath = os.path.join(patPath, patPath[-8:] + '_ProCurvaturesDataResults.txt')
+	patId = patPath[-8:]
+	supPath = os.path.join(patPath, patId + '_SupCurvaturesDataResults.txt')
+	proPath = os.path.join(patPath, patId + '_ProCurvaturesDataResults.txt')
 	supIn = open(supPath, 'r')
 	supLines = supIn.readlines()
 	supIn.close()
@@ -159,8 +223,23 @@ def compareSupineProne(patPath):
 	proIn.close()
 	
 	outLines = []
-	outLines.append(',Supine,Prone')
-	outLines.append('{},{},{}'.format('Mean Curvatures', supLines[0].strip().split(', ')[1], supLines[0].strip(', ').split()[1]))
+	outLines.append('{},Supine,Prone'.format(patId))
+	#outLines.append('{},{},{}'.format('Mean Curvatures', supLines[0].strip().split(', ')[1], supLines[0].strip(', ').split()[1]))
+	
+	for y in range(0,6):
+		outLines.append('{},{},{}'.format(supLines[y].strip().split(', ')[0], supLines[y].strip().split(', ')[1], proLines[y].strip().split(', ')[1]))
+	
+	outLines.append(supLines[6].strip())
+	
+	for y in range(7,12):
+		outLines.append('{},{},{}'.format(supLines[y].strip().split(', ')[0], supLines[y].strip().split(', ')[1], proLines[y].strip().split(', ')[1]))
+	
+	outLines.append(supLines[12].strip())
+
+	for y in range(13, 38, 3):
+		outLines.append('{},{},{}'.format(supLines[y].strip().split(', ')[0], supLines[y].strip().split(', ')[1], proLines[y].strip().split(', ')[1]))
+		outLines.append('{},{},{}'.format(supLines[y+1].strip().split(', ')[0], supLines[y+1].strip().split(', ')[1], proLines[y+1].strip().split(', ')[1]))
+	
 	
 	outPath = os.path.join(patPath, patPath[-8:]+'_BothCurvaturesDataResults.txt')
 	fOut = open(outPath, 'w')
@@ -177,7 +256,7 @@ getStats(r"C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\TEST0012\TES
 compareSupineProne(r'C:\Users\jlaframboise\Documents\ColonCurves_JL\CtVolumes\TEST0012')
 	
 	
-	
+#getStats(r
 	
 	
 	
